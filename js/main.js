db73580650e0d1cd285e799e6fe6ccd2086b6d8b
Bix <36926011+bixbite2@users.noreply.github.com>
2018-04-15 17:53:32 -0300

    botões ainda não funcionais porém bugs arrumados

--- js/main.js
+++ js/main.js
@@ -1,24 +1,93 @@
 console.log('main');
 
-const p = document.querySelector('form');
-console.log(p);
+const p = document.querySelectorAll('form')[2];
+// console.log(p);
 
+
+let tab = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+];
 
 let player = 'x';
+let win = '';
 p.addEventListener('click', function(e) {
     e.preventDefault();
-    if (player === 'x') {
-        if (e.target.tagName === 'BUTTON') {
-            console.log(parseInt(e.target.id));
-            p[parseInt(e.target.id)].innerHTML = `<img src ="letter-x.png" alt ="X" width ="50%" height ="50%" >`;
+    let x = '<img src="letter-x.png" alt="X" width="50%" height="50%">';
+    let o = '<img src="circle.png" alt="O" width="50%" height="50%">';
+
+    let line = parseInt(e.target.id);
+
+    if (player === 'x') { // podia ter feito um switch, mas não vejo muita necessidade
+        if (line === 0 && tab[0][0] !== 'o') {
+            tab[0][0] = 'x';
         }
-        player = 'o';
+        if (line === 1 && tab[0][1] !== 'o') {
+            tab[0][1] = 'x';
+        }
+        if (line === 2 && tab[0][2] !== 'o') {
+            tab[0][2] = 'x';
+        }
+        if (line === 3 && tab[1][0] !== 'o') {
+            tab[1][0] = 'x';
+        }
+        if (line === 4 && tab[1][1] !== 'o') {
+            tab[1][1] = 'x';
+        }
+        if (line === 5 && tab[1][2] !== 'o') {
+            tab[1][2] = 'x';
+        }
+        if (line === 6 && tab[2][0] !== 'o') {
+            tab[2][0] = 'x';
+        }
+        if (line === 7 && tab[2][1] !== 'o') {
+            tab[2][1] = 'x';
+        }
+        if (line === 8 && tab[2][2] !== 'o') {
+            tab[2][2] = 'x';
+        }
+        if (e.target.tagName === 'BUTTON' && e.target.innerHTML === '') {
+            // console.log(parseInt(e.target.id));
+            p[parseInt(e.target.id)].innerHTML = x;
+            console.table(tab);
+            player = 'o';
+        }
     } else {
-        if (e.target.tagName === 'BUTTON') {
-            console.log(parseInt(e.target.id));
-            p[parseInt(e.target.id)].innerHTML = `<img src ="circle.png" alt ="X" width ="50%" height ="50%" >`;
+        if (line === 0 && tab[0][0] !== 'x') {
+            tab[0][0] = 'o';
         }
-        player = 'x';
+        if (line === 1 && tab[0][1] !== 'x') {
+            tab[0][1] = 'o';
+        }
+        if (line === 2 && tab[0][2] !== 'x') {
+            tab[0][2] = 'o';
+        }
+        if (line === 3 && tab[1][0] !== 'x') {
+            tab[1][0] = 'o';
+        }
+        if (line === 4 && tab[1][1] !== 'x') {
+            tab[1][1] = 'o';
+        }
+        if (line === 5 && tab[1][2] !== 'x') {
+            tab[1][2] = 'o';
+        }
+        if (line === 6 && tab[2][0] !== 'x') {
+            tab[2][0] = 'o';
+        }
+        if (line === 7 && tab[2][1] !== 'x') {
+            tab[2][1] = 'o';
+        }
+        if (line === 8 && tab[2][2] !== 'x') {
+            tab[2][2] = 'o';
+        }
+        if (e.target.tagName === 'BUTTON' && e.target.innerHTML === '') {
+            // console.log(parseInt(e.target.id));
+            p[parseInt(e.target.id)].innerHTML = o;
+            console.table(tab);
+            player = 'x';
+        }
     }
-    console.log(player);
+    // console.log(line);
+    // console.log(player);
 });
